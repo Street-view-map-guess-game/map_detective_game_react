4969293ed20a1a3e86bab75d3a646b92132870fd
Ahmet Talha TURKMEN <75725469+AhmetTalhaTURKMEN@users.noreply.github.com>
2023-02-21 17:15:06 +0300

    Add files via upload

--- src/card/card.js
+++ src/card/card.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Card = ({ title, imageUrl, imageAlt, description }) => {
+const Card = ({ title, imageUrl, imageAlt, description, countryName }) => {
     return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img className="w-full" src={imageUrl} alt={imageAlt} />
-            <div className="px-6 py-4">
-                <div className="text-center font-bold text-xl mb-2">{title}
-                    <p className="text-gray-700 text-base">{description}</p>
-                    <Link to="/gamescreen">
-                        <button class="bg-transparent hover:bg-green-700 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-700 hover:border-transparent rounded">
-                            Start Game
-                        </button>
-                    </Link>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg relative hover:scale-110 transition duration-500">
+            <img className="object-cover h-96 w-96 hover:object-scale-up" src={imageUrl} alt={imageAlt} />
+            <div className="px-6 py-4 absolute bottom-0 left-0 w-full bg-black bg-opacity-50 text-white">
+                <div className="font-bold text-xl mb-2 text-center">
+                    {title}
+                    <p className="text-gray-200 text-base">{description}</p>
+                    <div className="mt-4">
+                        <Link to={{
+                            pathname: '/gamescreen',
+                            state: { gameId: { countryName } }
+                        }}>
+                            <button className="bg-purple-600 hover:bg-purple-800 text-white font-bold py-2 px-4 rounded">
+                                Start Game
+                            </button>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/CountrySelectionPage.js
+++ src/pages/CountrySelectionPage.js
@@ -4,15 +4,24 @@ import Card from '../card/card';
 function CountrySelectionPage() {
     return (
         <div>
-            <div className="flex justify-center">
+            <div className="flex space-x-6">
                 <Card
                     title="TURKEY"
                     imageUrl="https://upload.wikimedia.org/wikipedia/commons/0/01/K%C4%B1z_Kulesi_February_2013_01.jpg"
                     imageAlt="Maiden's Tower"
                     description="Turkey is great country bla bla..."
+                    countryName="turkey"
+                />
+                <Card
+                    title="FRANCE"
+                    imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Tour_Eiffel_Wikimedia_Commons_%28cropped%29.jpg/640px-Tour_Eiffel_Wikimedia_Commons_%28cropped%29.jpg"
+                    imageAlt="Eiffel Tower"
+                    description="FRANCE is great country bla bla..."
+                    countryName="france"
                 />
             </div>
-            
+
+
         </div>
     );
 }
