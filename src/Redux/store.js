2ca9b9cbd3b038153ea889d88647de4b9625f1af
Ahmet Talha TURKMEN <75725469+AhmetTalhaTURKMEN@users.noreply.github.com>
2023-02-22 22:01:43 +0300

    Delete src directory

--- src/Redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import mapSlice from "./MapGameSlices/mapSlice";
-
-export const store = configureStore({
-  reducer: {
-    mapSlc: mapSlice,
-  },
-});
