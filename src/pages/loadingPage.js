470944e359830db6c81c25c445a7d18a44b38293
Ahmet Talha TURKMEN <75725469+AhmetTalhaTURKMEN@users.noreply.github.com>
2023-02-23 10:33:24 +0300

    Add files via upload

--- src/pages/loadingPage.js
+++ src/pages/loadingPage.js
@@ -6,12 +6,12 @@ import mapdetectivelogo from "../assets/images/pageImage/worldlogo.png"
 export default function Loadingpage() {
 
   return (
-    <div class="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-lime-600 via-green-800 to-neutral-900 min-h-screen flex justify-center items-center" style={{
+    <div className="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-lime-600 via-green-800 to-neutral-900 min-h-screen flex justify-center items-center" style={{
       width: "100%",
       height: "100%"
     }} >
-      <img style={{ marginBottom: "40px", marginLeft: "5px" }} src={mapdetectivelogo} alt="logo" width={"230rem"} class='absolute  opacity-60' />
-      <img src={loadingscroll} alt="logo" width={"250rem"} class='opacity-90   absolute animate-spin transition duration-500' />
+      <img style={{ marginBottom: "40px", marginLeft: "5px" }} src={mapdetectivelogo} alt="logo" width={"230rem"} className='absolute  opacity-60' />
+      <img src={loadingscroll} alt="logo" width={"250rem"} className='opacity-90   absolute animate-spin transition duration-500' />
     </div>
   )
 }
