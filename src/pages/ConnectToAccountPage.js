59744e0e2fec9eaeab27ffafb32842cb2ce86999
abdulvahap51 <79311293+abdulvahap51@users.noreply.github.com>
2023-02-27 10:35:19 +0300

    Add files via upload

--- src/pages/ConnectToAccountPage.js
+++ src/pages/ConnectToAccountPage.js
@@ -1,12 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import { GoogleButton } from "react-google-button";
 import { UserAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/images/pageImage/siteLogo.png";
+import Loadingpage from "./LoadingPage";
 
 export default function ConnectToAccountPage() {
   const { googleSignIn, user } = UserAuth();
   const navigate = useNavigate();
+  const [time,settime] = useState(false)
 
   const handleGoogleSignIn = async () => {
     try {
@@ -17,28 +19,33 @@ export default function ConnectToAccountPage() {
   };
 
   useEffect(() => {
-    if (user != null) {
-      navigate("/countryselection");
-    }
+    setTimeout(() => {
+      settime(true)
+      if (user) {
+        navigate("/countryselection");
+      }
+    }, 1000);
+    
+   
   }, [user]);
-  return (
-    <div className="absolute z-1 h-screen w-screen bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-red-500 via-sky-800 to-red-900 ">
-      <div
-        style={{
-          boxShadow:
-            "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
-        }}
-        className=" group absolute sm:w-full sm:overflow-hidden h-72 w-76 bg-white rounded-md  z-10 top-1/2 left-1/2 -translate-x-1/2  -translate-y-1/2 flex items-center justify-center">
-        <div>
-          <img
-            className="object-fit opacity-100 group-hover:opacity-0 py-8 px-12  h-72 w-76 hover:scale-150 transition duration-500"
-            src={logo}></img>
-        </div>
-        <div class=" absolute translate-y-8  opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100 duration-500">
-          <p class="text-2xl font-bold	 text-black"></p>
-          <GoogleButton onClick={handleGoogleSignIn} />
-        </div>
-      </div>
+  return ( time ? (<div className="absolute z-1 h-screen w-screen bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-red-500 via-sky-800 to-red-900 ">
+  <div
+    style={{
+      boxShadow:
+        "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+    }}
+    className=" group absolute sm:w-full sm:overflow-hidden h-72 w-76 bg-white rounded-md  z-10 top-1/2 left-1/2 -translate-x-1/2  -translate-y-1/2 flex items-center justify-center">
+    <div>
+      <img
+        className="object-fit opacity-100 group-hover:opacity-0 py-8 px-12  h-72 w-76 hover:scale-150 transition duration-500"
+        src={logo}></img>
     </div>
+    <div class=" absolute translate-y-8  opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100 duration-500">
+      <p class="text-2xl font-bold	 text-black"></p>
+      <GoogleButton onClick={handleGoogleSignIn} />
+    </div>
+  </div>
+</div>):(<Loadingpage></Loadingpage>)
+    
   );
 }
