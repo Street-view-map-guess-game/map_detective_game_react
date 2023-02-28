96a2349831f7b714dcbec9b96a38c3790e5d030c
Ahmet Talha TURKMEN <75725469+AhmetTalhaTURKMEN@users.noreply.github.com>
2023-02-28 20:12:32 +0300

    Add files via upload

--- src/gameFunctions/gameFunctions.js
+++ src/gameFunctions/gameFunctions.js
@@ -1,4 +1,25 @@
 export const setScore = (distance) => {
-  var score = (50000 / distance).toFixed(2);
+
+  const MAX_SCORE = 10000;
+  let score = 0;
+  console.log(distance)
+
+  if (distance <= 355) {
+    score = -(Math.pow(distance, 2) / 20) + 10000;
+  }
+  else if (distance > 355 && distance <= 660) {
+    score = -(Math.pow(distance, 2) / 100) + 5000;
+  }
+  else if (distance > 660 && distance <= 800) {
+    score = -(Math.pow(distance, 2) / 500) + 1500;
+  }
+  else if (distance > 800) {
+    score = -(Math.pow(distance, 2) / 500000) + 250;
+  }
+  if (score <= 0) {
+    score = 10;
+  }
+
   return score;
+
 };
