6a1cf49ecfecb4c91ff9729979226e3d5945cf07
Ahmet Talha TURKMEN <75725469+AhmetTalhaTURKMEN@users.noreply.github.com>
2023-03-03 21:22:43 +0300

    Add files via upload

--- src/components/Compass.js
+++ src/components/Compass.js
@@ -1,27 +1,20 @@
 import React, { useState, useEffect } from "react";
 
-import "font-awesome/css/font-awesome.min.css";
+import compassNeddle from "../assets/images/pageImage/404error/compass_neddle.png"
 
 function Compass({ panorama }) {
     const [heading, setHeading] = useState(panorama.getPov().heading);
-    const [direction, setDirection] = useState("East");
+    const [isRotating, setIsRotating] = useState(false);
 
     useEffect(() => {
-        const povChangeListener = window.google.maps.event.addListener(panorama, 'pov_changed', function () {
-            const pov = panorama.getPov();
-            setHeading(pov.heading);
-
-            if (pov.heading >= 315 || pov.heading < 45) {
-                setDirection("East");
-            } else if (pov.heading >= 45 && pov.heading < 135) {
-                setDirection("North");
-            } else if (pov.heading >= 135 && pov.heading < 225) {
-                setDirection("West");
-            } else if (pov.heading >= 225 && pov.heading < 315) {
-                setDirection("South");
+        const povChangeListener = window.google.maps.event.addListener(
+            panorama,
+            "pov_changed",
+            function () {
+                const pov = panorama.getPov();
+                setHeading(pov.heading);
             }
-        });
-
+        );
         return () => {
             window.google.maps.event.removeListener(povChangeListener);
         };
@@ -29,10 +22,92 @@ function Compass({ panorama }) {
 
     const compassRotation = `rotate(${heading}deg)`;
 
+    const handleMouseDown = (event) => {
+        setIsRotating(true);
+    };
+
+    const handleMouseUp = (event) => {
+        setIsRotating(false);
+    };
+
+    // basılı tutma ile
+    const handleMouseMove = (event) => {
+        if (isRotating) {
+            const compass = event.currentTarget;
+            const rect = compass.getBoundingClientRect();
+            const centerX = rect.left + rect.width / 2;
+            const centerY = rect.top + rect.height / 2;
+            const clickX = event.clientX;
+            const clickY = event.clientY;
+            const deltaX = clickX - centerX;
+            const deltaY = centerY - clickY;
+            const angle = Math.atan2(deltaY, deltaX) * 180 / Math.PI;
+            const newHeading = -(angle - 90);
+
+            setHeading(newHeading);
+
+            const pov = panorama.getPov();
+            pov.heading = newHeading;
+            panorama.setPov(pov);
+        }
+    };
+    //tek tıklama ile 
+    const handleCompassClick = (event) => {
+        const compass = event.currentTarget;
+        const rect = compass.getBoundingClientRect();
+        const centerX = rect.left + rect.width / 2;
+        const centerY = rect.top + rect.height / 2;
+        const clickX = event.clientX;
+        const clickY = event.clientY;
+        const deltaX = clickX - centerX;
+        const deltaY = centerY - clickY;
+        const angle = Math.atan2(deltaY, deltaX) * 180 / Math.PI;
+        const newHeading = -(angle - 90);
+
+        setHeading(newHeading);
+
+        const pov = panorama.getPov();
+        pov.heading = newHeading;
+        panorama.setPov(pov);
+    };
+
     return (
-        <div style={{ position: 'absolute', top: 10, right: 10, zIndex: 9999, fontSize: 80 }}>
-            <i className="fa fa-compass" style={{ transform: compassRotation }}></i>
-            {direction}
+        <div
+            style={{
+                position: "absolute",
+                top: 25,
+                right: 25,
+                zIndex: 9999,
+                fontSize: 80,
+            }}
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
+            onMouseMove={handleMouseMove}
+            onClick={handleCompassClick}
+        >
+            <img
+                src={compassNeddle}
+                alt="compass needle"
+                width="100"
+                height="100"
+                style={{ transform: compassRotation }}
+            />
+            <div
+                style={{
+                    userSelect: "none",
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                    borderRadius: "50%",
+                    background: "rgba(0,0,0,0.4)",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+            </div>
         </div>
     );
 }
