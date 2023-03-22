1d868e351251e471ff3ffde805e2a0696ef3c776
ErdalNayir <19360859049@ogrenci.btu.edu.tr>
2023-03-22 18:29:43 +0300

    Responsive Design

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,11 +17,35 @@ function Header() {
   };
 
   return (
-    <div className="navbar bg-black h-20 top-0 z-10 fixed">
+    <div className="navbar bg-black h-20 top-0 z-10 fixed ">
+      <div class="dropdown">
+        <label tabindex="0" class="btn btn-ghost hidden sm:inline">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="white">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
+        </label>
+        <ul
+          tabindex="0"
+          class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-40">
+          <li>
+            <DropdownMenu display={"inline"} />
+          </li>
+        </ul>
+      </div>
       <Link to={"/gamemodpage"}>
-        <div className="w-24 flex">
+        <div className="w-24 flex sm:navbar-center ">
           <img
-            className="absolute top-5 left-5 opacity-80"
+            className="absolute top-5 left-5 sm:left-12 sm:top-6 opacity-80"
             width="150"
             src={whiteLogo}
             alt="logo"
@@ -29,18 +53,16 @@ function Header() {
         </div>
       </Link>
       <div className="flex flex-grow justify-center">
-        <DropdownMenu />
+        <DropdownMenu display={"hidden"} />
       </div>
       <div className="flex">
         <button
           className="bg-transparent hover:bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-red-500 via-sky-800 to-red-900 text-white font-bold py-2 px-4 rounded border border-white"
-          onClick={handleSignOut}
-        >
+          onClick={handleSignOut}>
           Logout
         </button>
       </div>
     </div>
-
   );
 }
 
